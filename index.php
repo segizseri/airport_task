b5fa10156f3c3f5a57e8a356ebf70ac89e7bacee
segizseri <segizseri@gmail.com>
2023-02-07 02:08:26 +0600

    Push task

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php 
+
+$flights = flights();
+
+$searchRoute = searchRoute($flights, 'VKO');
+
+$routes = getRoute();
+
+$longRoute = longRoute($routes);
+
+
+echo json_encode($longRoute);
+
+
+function getRoute()
+{
+	$flights = flights();
+	$result = [];
+	$duplicateKey = null;
+	foreach ($flights as $key => $flight) {
+		foreach ($flights as $childKey => $childFlight) {
+			if ($flight['to'] === $childFlight['from']) {
+				if (strtotime($flight['arrival']) < strtotime($childFlight['depart'])) {
+					$flight['arrival'] = $childFlight['arrival'];
+					$duplicateKey = $childKey;
+				}
+			}
+		}
+		if ($key !== $duplicateKey) {
+			$result[] = $flight;	
+		}
+	}
+	return $result;
+}
+
+function longRoute($flights): array
+{
+	$route = [];
+
+
+	foreach ($flights as $key => $flight) {
+		$arrival = strtotime($flight['arrival']);
+		$depart = strtotime($flight['depart']);
+
+		$differentTime = ABS($arrival - $depart);
+		$route[$key] = $differentTime;
+	}
+	if (empty($route)) {
+		return $route;
+	}
+
+	$key = array_search(max($route), $route);
+
+	return $flights[$key];
+}
+
+function searchRoute($flights, $city = null)
+{
+	$result = [];
+	foreach ($flights as $key => $flight) {
+		if ($flight['from'] === $city || $flight['to'] === $city) {
+			$result[] = $flight;
+		}
+	}
+	return $result;
+}
+
+function group_by($key, $data) {
+    $result = array();
+
+    foreach($data as $val) {
+        if(array_key_exists($key, $val)){
+            $result[$val[$key]][] = $val;
+        }else{
+            $result[""][] = $val;
+        }
+    }
+
+    return $result;
+}
+
+function flights()
+{
+	return [
+	[
+		'from' => 'VKO',
+		'to' => 'DME',
+		'depart' => '01.01.2020 12:44',
+		'arrival' => '01.01.2020 13:44'
+	],
+	[
+		'from' => 'DME',
+		'to' => 'JFK',
+		'depart' => '02.01.2020 23:00',
+		'arrival' => '03.01.2020 11:44'
+	],
+	[
+		'from' => 'DME',
+		'to' => 'HKT',
+		'depart' => '01.01.2020 13:40',
+		'arrival' => '01.01.2020 22:22'
+	]		
+];
+}
+
+?>
